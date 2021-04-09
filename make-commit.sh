45f43dd4c65d2b4e7be5add4a9299d655b051837
MijazzChan <mijazz@qq.com>
2021-04-09 22:38:05 +0800

    Fri Apr  9 22:38:04 CST 2021 - ProblemCount: 19

---
+++ make-commit.sh
@@ -0,0 +1,39 @@
+fileList=()
+problemCount=0
+
+for file in ./src/leetcode/editor/en/*; do
+  if [ -f "$file" ] && [[ "$file" == *Prbl*.java ]]; then
+    fileList=("${fileList[@]}" "$file")
+    ((problemCount++))
+  fi
+done
+
+sed "s/REPLACEME/$problemCount/" ./README_TEMPLATE.md >./README.md
+
+if [ -f "./table.temp" ]; then
+  rm "./table.temp"
+fi
+
+for eachLeetcode in "${fileList[@]}"; do
+  problemNum=$(echo "$eachLeetcode" | grep -Eo 'Prbl[[:digit:]]{1,5}' | sed 's/Prbl/Problem /' | xargs)
+  problemName=$(grep '^//[[:blank:]]*java:' "$eachLeetcode" | cut -d':' -f2 | xargs)
+  problemFileName=$(echo "$eachLeetcode" | grep -Eo 'Prbl[[:digit:]]{1,5}[a-zA-Z]*\.java$' | xargs)
+  echo -e "| $problemNum | $problemName | [$problemFileName]($eachLeetcode) |" >>./table.temp
+done
+
+sort -k2n -t'|' ./table.temp >>./README.md
+
+git add -A
+column -t -s'|' ./table.temp
+
+if [ -f "./table.temp" ]; then
+  rm "./table.temp"
+fi
+
+read -r -p "Commit? [y/N] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  git commit -S -m "$(date) - ProblemCount: $problemCount"
+  exit 0
+else
+  exit 0
+fi
